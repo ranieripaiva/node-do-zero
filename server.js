b5cca72c581fd43bbdeaeb178b4ce349d0a825ba
ranieripaiva <63205834+ranieripaiva@users.noreply.github.com>
2025-03-01 18:59:11 -0300

    git branch -M main

--- server.js
+++ server.js
@@ -60,6 +60,7 @@ server.delete('/videos/:id', async (request, reply) => {
 })
 
 server.listen({
+    host: '0.0.0.0',
     port: process.env.PORT ?? 3333
 })
 //https://github.com/ranieripaiva/node-do-zero.git
